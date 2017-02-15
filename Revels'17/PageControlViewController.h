8c952a4c1f47668d3923583b5e18053796934460
Abhishek Vora <abhivora8@gmail.com>
2017-02-15 22:18:01 +0530

    Pageviewcontroller added to Categories

---
+++ Revels'17/PageControlViewController.h
@@ -0,0 +1,13 @@
+//
+//  PageControlViewController.h
+//  Revels'17
+//
+//  Created by Abhishek Vora on 05/02/17.
+//  Copyright © 2017 Abhishek Vora. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageControlViewController : UIViewController
+
+@end
